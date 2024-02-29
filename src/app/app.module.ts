136e4f213e46ac70534b166347b265af591df156
Andrey Timoshenkov <andreytmshnkv@gmail.com>
2024-02-29 23:07:18 +0300

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { TuiRootModule } from "@taiga-ui/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+      BrowserAnimationsModule,
+      TuiRootModule
+],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
